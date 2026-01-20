a40e781ee3a764d742951425f38bdbf6e7b1e14f
0rgen <jhosua_saa2412@hotmail.com>
2026-01-20 14:26:20 -0500

    Update command-line argument handling for benchmarks
    Updated the arguments for main_benchmark, now it receive parameters to run the disk and cache benchmarks

--- src/main_benchmark.c
+++ src/main_benchmark.c
@@ -6,41 +6,45 @@
 
 int main(int argc, char *argv[]) {
 
-    if (argc < 2) {
+    if (argc < 3) {
         printf("Uso:\n");
-        printf("  %s disk\n", argv[0]);
-        printf("  %s cache\n", argv[0]);
+        printf("Uso: %s [disk size_file|\n"
+               "         cache min_bytes max_bytes factor accesos]\n", argv[0]);
         return EXIT_FAILURE;
-    }
+        }
 
     if (strcmp(argv[1], "disk") == 0) {
         printf("Ejecutando benchmark de disco...\n");
 
-        int sizes_mb[] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
-        int n = sizeof(sizes_mb) / sizeof(int);
-
-        for (int i = 0; i < n; i++) {
-            size_t size_bytes = sizes_mb[i] * 1024 * 1024;
-            printf("  Tamaño: %d MB\n", sizes_mb[i]);
-            run_disk_benchmark(
-                "test_files/test_file1.bin",
-                size_bytes,
-                "results/disk_results.csv"
-            );
+        if(argc != 3) {
+            printf("Uso: %s disk size_file\n", argv[0]);
+            return EXIT_FAILURE;
+        } else if (atoi(argv[2]) <= 0) {
+            printf("El tamaño del archivo debe ser un entero positivo.\n");
+            return EXIT_FAILURE;
+        }else {
+            printf("Ejecutando benchmark de disco...\n");
+            int size_file = atoi(argv[2]);
+            run_disk_benchmark("test_files/test_file1.bin", size_file, "results/disk_results.csv");
         }
 
     } else if (strcmp(argv[1], "cache") == 0) {
+        
+        printf("Ejecutando benchmark de caché...\n");
 
-        run_mem_cache_bench(
-            "results/cache_results.csv",
-            4 * 1024,
-            64 * 1024 * 1024,
-            2,
-            64000000
-        );
+        if(argc !=6) {
+            printf("Uso: %s cache min_bytes max_bytes factor accesos\n", argv[0]);
+            return EXIT_FAILURE;
+        } else if (atoi(argv[2]) <= 0 || atoi(argv[3]) <= 0 || atoi(argv[4]) <= 1 || atoi(argv[5]) <= 0) {
+            printf("Todos los parámetross deben ser enteros positivos, con factor mayor a 1.\n");
+            return EXIT_FAILURE;
+        }else {
+            printf("Ejecutando benchmark de caché...\n");
+            run_mem_cache_bench("results/cache_results.csv", atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+        }
 
     } else {
-        printf("Opción no válida\n");
+        printf("Opción no válida: %s\n", argv[1]);
     }
 
     return EXIT_SUCCESS;
